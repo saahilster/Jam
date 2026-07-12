7673f27ce0483d8492b49e67571a427f2254e7ab
duckayy <legaspiashton@gmail.com>
2026-07-11 20:01:09 -0700

    Update EnemyData.cs

--- My project/Assets/Scripts/Monsters/EnemyData.cs
+++ My project/Assets/Scripts/Monsters/EnemyData.cs
@@ -3,5 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
+    public string enemyName;
+    public Sprite enemySprite;
+    //public MovementType movementType;
+
+    //detection
+    public float detectionRadius;
+    public float fieldOfViewAngle;
+    public LayerMask obstacleLayerMask;
+    public float lostInterestTime;
     
+    public int damageToPlayer;
+    //audio
+    public AudioClip[] footstepSounds;
+    public AudioClip detectionSound;
+    public AudioClip attackSound;
+
+    //Animation
+    public Sprite[] walkFrames;
 }
