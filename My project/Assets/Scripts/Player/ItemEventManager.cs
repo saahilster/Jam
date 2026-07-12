33dd774e731295cf883ca9ebb3ee3ffc94704850
saahilster <saahilfardhook@gmail.com>
2026-07-12 15:27:14 -0700

    working on shovel

---
+++ My project/Assets/Scripts/Player/ItemEventManager.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class ItemEventManager : MonoBehaviour
+{
+    public bool hasShovel = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shovel"))
+        {
+            hasShovel = true;
+            Destroy(other.gameObject);
+        }
+    }
+}
