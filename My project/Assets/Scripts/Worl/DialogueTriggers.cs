301149fd466169c83cda28a085a73dfb5b49ef7c
saahilster <saahilfardhook@gmail.com>
2026-07-13 14:34:00 -0700

    2 rooms done
    Mother and cave in room finished

---
+++ My project/Assets/Scripts/Worl/DialogueTriggers.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueTriggers", menuName = "Scriptable Objects/DialogueTriggers")]
+public class DialogueTriggers : ScriptableObject
+{
+    public string dialogue;   
+}
