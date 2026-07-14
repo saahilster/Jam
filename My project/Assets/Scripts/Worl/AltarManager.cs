92499aef44819a5b20cdb33d84472e9ff41440ff
saahilster <saahilfardhook@gmail.com>
2026-07-13 23:09:30 -0700

    ending path done.

---
+++ My project/Assets/Scripts/Worl/AltarManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AltarManager : MonoBehaviour
+{
+    [SerializeField] List<AltarSlot> slots = new List<AltarSlot>{};
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckWin();
+    }
+
+    public void CheckWin()
+    {
+        if (slots[0].currentSelected == 0 &&slots[1].currentSelected == 1  && slots[2].currentSelected == 2)
+        {
+            Debug.Log("Fin.");
+        }
+    }
+}
