035ca0e3fc9f33b3de115da9674454e2b08e003f
saahilster <saahilfardhook@gmail.com>
2026-07-14 22:20:07 -0700

    mask created.

--- My project/Assets/Scripts/Player/ItemEventManager.cs
+++ My project/Assets/Scripts/Player/ItemEventManager.cs
@@ -1,20 +1,33 @@
+using System.Collections;
 using NUnit.Framework;
+using TMPro;
 using UnityEngine;
 
 public class ItemEventManager : MonoBehaviour
 {
     public bool hasShovel = false;
     public bool hasMask = false;
+    [SerializeField] private GameObject responseObj;
+    [SerializeField] private TextMeshProUGUI response;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator ActivateText(string say)
+    {
+        responseObj.SetActive(true);
+        response.text = say;
+        yield return new WaitForSeconds(4);
+        response.text = null;
+        responseObj.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
@@ -23,8 +36,10 @@ public class ItemEventManager : MonoBehaviour
         {
             hasShovel = true;
             Destroy(other.gameObject);
+            StartCoroutine(ActivateText("Grabbed a shovel."));
+
         }
-        else if(other.gameObject.tag == "Rock" && hasShovel)
+        else if (other.gameObject.tag == "Rock" && hasShovel)
         {
             Debug.Log("worked");
             Destroy(other.gameObject);
@@ -32,6 +47,8 @@ public class ItemEventManager : MonoBehaviour
         else if (other.gameObject.CompareTag("Mask"))
         {
             hasMask = true;
+            Destroy(other.gameObject);
+            StartCoroutine(ActivateText("Grabbed the mask"));
             Debug.Log("mask obtained.");
         }
     }
