1bd4ac9aef3b634f29c95b888ed56e8cc1d78807
duckayy <legaspiashton@gmail.com>
2026-07-17 17:18:38 -0700

    child

---
+++ My project/Assets/Scripts/Monsters/ChildBehavior.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChildBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform waypointsParent;
+    private NavMeshAgent agent;
+    /// <summary>
+    /// Get waypoints, upon looking inflict affect on player and flee to a specific location and wait
+    /// </summary>
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
